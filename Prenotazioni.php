7dee9f9719d16ff30b38340ff47d83f18f58cf4a
ZuccalaJacopo <56387357+ZuccalaJacopo@users.noreply.github.com>
2021-03-06 12:29:26 +0100

    Add SQL insertion example

--- Prenotazioni.php
+++ Prenotazioni.php
@@ -1,2 +1,32 @@
 <?php
-ini_set("DEBUG","1");
+
+ini_set('display-error', 1);
+ini_set('log-error', 0);
+
+$host = 'localhost';
+$db = 'prenotazioni';
+$user = 'root';
+$pass = '';
+
+//stringe la connessione
+$dsn = 'mysql:host=' . $host . 'dbname=' . $db;
+
+$pdo = new PDO ($dsn, $user, $pass);
+
+//variabili al momento costanti, poi verranno prese tramite POST
+$codice_fiscale = 'BTGLSN00N00B157H';
+$giorno = ''2021-03-06;
+
+//Query di inserimento preparata
+$sql = "INSERT INTO prenotazioni VALUES(null, :codice_fiscale, :giorno)";
+
+//inviamo la query al database che la tiene in pancia
+$stmt = $pdo -> prepare($sql);
+
+//inviamo i dati co
+$stmt->execute(
+    [
+        'codice_fiscale' => $codice_fiscale,
+        'giorno' => $giorno
+    ]
+);

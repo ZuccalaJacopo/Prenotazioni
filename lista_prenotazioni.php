c626a6ba4f42913a7f4e0a7d73287b04085c5708
ZuccalaJacopo <56387357+ZuccalaJacopo@users.noreply.github.com>
2021-03-20 10:28:07 +0100

    update

---
+++ lista_prenotazioni.php
@@ -0,0 +1,23 @@
+
+<?php
+
+require 'vendor/autoload.php';
+include_once 'config.php';
+
+//use League\Plates\Engine;
+
+//Viene creato l'oggetto per la gestione dei template
+$templates = new League\Pltes\Engine('./view','tpl');
+
+//Query per recuperare tutte le prenotazioni
+$sql = "SELECT * FROM prenotazioni";
+
+//Invio la query al server MySQL
+$stmt = $pdo->query($sql);
+
+//Estraggo le righe di risposta che finiranno come vettori in $result
+$result = $stmt->fetchAll();
+
+//Rendo un template che mi visualizza la tabella
+echo $templates->render('lista_prenotazioni', ['result' => $result]);
+
